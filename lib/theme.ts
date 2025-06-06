456a78a61c4f0931770eecac963dfe60098e6a6f
MyPointViews <leonardomazzone94@gmail.com>
2025-06-06 15:00:57 +0200

    Create theme.ts

---
+++ lib/theme.ts
@@ -0,0 +1,22 @@
+// lib/theme.ts
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#a47864',
+    },
+    secondary: {
+      main: '#c69d8f',
+    },
+    background: {
+      default: '#fffaf5',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+})
+
+export default theme
