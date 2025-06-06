8ee36826455f4791808d222fbb38a69290fc5b2f
MyPointViews <leonardomazzone94@gmail.com>
2025-06-06 15:02:17 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#a47864',
+        secondary: '#c69d8f',
+      },
+    },
+  },
+  plugins: [],
+}
